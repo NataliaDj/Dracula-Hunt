c7ea394542b4f75c38918f13c55c0ceeb734d842
mishphan <mishi.me@hotmail.com>
2014-10-14 15:59:06 +1100

    drac

--- dracula.c
+++ dracula.c
@@ -6,25 +6,48 @@
 #include "Game.h"
 #include "DracView.h"
 
+#define PLACES 12
+
 void decideDraculaMove(DracView gameState)
 {
-	//Get some information from game state
+	//Get some information from game statec
    //PlayerID player = whoAmI(gameState);
    //LocationID currLocation = whereIs(gameState, player);
-   int *numLocations = NULL;
-   LocationID *locations = whereCanIgo(gameState, numLocations, TRUE,TRUE);
    Round r = giveMeTheRound(gameState);
 
-   godalmingLocation = whereIs(gameState, PLAYER_LORD_GODALMING);
-   //etc
+   //LocationID godLocation = whereIs(gameState, PLAYER_LORD_GODALMING);
+   //LocationID sewLocation = whereIs(gameState, PLAYER_DR_SEWARD);
+   //LocationID vanLocation = whereIs(gameState, PLAYER_VAN_HELSING);
+   //LocationID minLocation = whereIs(gameState, PLAYER_MINA_HARKER);
+   LocationID places[PLACES] = {MARSEILLES, GENOA, MILAN, ZURICH, GENEVA, STRASBOURG, 
+      BRUSSELS, LE_HAVRE, NANTES, BORDEAUX, TOULOUSE};
+   
 
    if (r == 0) {
-      //Go furthest away from hunters
+      //Go furthest away from h
+      registerBestPlay("MR", "In Marseilles");
+      return;
       //Prefer port cities
       //registerBestPlay("CD", "NOTHING");
    }
+   int *numLocations = NULL;
+   LocationID *locations = whereCanIgo(gameState, numLocations, TRUE,FALSE);
+   int choice = locations[0];
+
+   for (int i = 0; i < PLACES; ++i)
+   {
+      for (int j = 0; j < *numLocations; ++i)
+      {
+         if (places[i]==locations[j])
+         {
+            choice = places[i];
+            break;
+         }
+      }
+   }
+   
 
    //Register bad move
-   char *move = idToAbbrev(*locations);
+   char *move = idToAbbrev(choice);
    registerBestPlay(move, "NOTHING");
 }
