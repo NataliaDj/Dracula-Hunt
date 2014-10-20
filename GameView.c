7d4aa04e7000cca0d738bc467d35b79255842ed1
NataliaDj <nataliadjohari@yahoo.com.sg>
2014-10-20 21:46:38 +1100

    Fixed the Adriatic Sea bug

--- GameView.c
+++ GameView.c
@@ -125,13 +125,15 @@ LocationID *connectedLocations(GameView currentView, int *numLocations,
      return locations;
   }
 
-  int j = 0;
+  int j = 0, i;
   LocationID *locations = malloc(NUM_MAP_LOCATIONS*sizeof(LocationID));
   LocationID visited[NUM_MAP_LOCATIONS];
 
-  int i;
   for (i = 0; i < NUM_MAP_LOCATIONS; ++i)
+  {
+    locations[i] = -1
     visited[i] = FALSE;
+  }
 
   if (player == PLAYER_DRACULA)
   {
